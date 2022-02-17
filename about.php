61b0c9cfcd71d91fa5f6fa182dfbd19547b57e5c
Yashika Gupta <yashikagupta082@gmail.com>
2022-02-17 12:24:29 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>SCHOLAR'S ASSESSMENT PROBE </title>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="col-sm-12 outer">
+  <div class="row">
+       <div class="col-sm-12 banner1">
+        <div class="row">
+          <div class="col-sm-12 header">
+       <div class="row">
+            <div class="col-sm-3 logo"style="padding-top:0%;"><!-- <img src="logo2.jfif" style="height:80px;width:80px; border:1px solid white;border-radius:50%;"/> --><h3>SCHOLARS' ASSESSMENT</h3><h4 style="color:red; margin-left:30%; padding-top: -50%;">PROBE</h4>
+      </div>
+            <div class="col-sm-9 menu">
+        <nav class="navbar navbar-default" id="menu">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php"><span class="fa fa-home"></span> <b>Home</b></a></li>
+          <li><a href="about.php"><span class="fa fa-user-circle"></span> <b>About</b></a></li>
+                  <li><a href="feedback.php"><span class="fa fa-user"></span> <b>Feedback</b></a></li>
+                    <li><a href="logout.php"><span class="fa fa-sign-out"></span> <b>Logout</b></a></li>
+    
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+        </div>
+        </div>
+        </div>
+<p style="margin-top:17%;margin-left: 20%; font-size: 22px; color:black;margin-right: 20%; text-align: center;"> SCHOLARS' ASSESSMENT PROBE can be defined as a game or brain tester to test knowledge. It can contain an element of competition when participants play against each other to get the highest score, which makes helps participants become more engaged. Companies and education usually want to test a specific area of knowledge rather than general knowledge. In this way, quizzes are an engaging method for companies and schools to measure growth in knowledge, abilities, and/or skills. Typically, a quiz is a short test and doesn’t have as great an impact on grades as a test. Quizzes are often given by teachers several times throughout a course. It’s an easy way to keep track of progress from both perspectives because it gives teachers and students a moment of reflection. Students can find the subjects they need to focus on, and teachers gain insight into the knowledge gaps.</p>
+
+
+</body>
+</html>
